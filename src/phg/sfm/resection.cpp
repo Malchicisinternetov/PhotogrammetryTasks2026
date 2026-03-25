3e2920bfba28d2f34d0ee80aa3ec50ad17947eb3
Malchicisinternetov <73699805+Malchicisinternetov@users.noreply.github.com>
2026-03-25 14:37:26 +0300

    increase trials 2

--- src/phg/sfm/resection.cpp
+++ src/phg/sfm/resection.cpp
@@ -99,7 +99,7 @@ namespace {
 
         // https://en.wikipedia.org/wiki/Random_sample_consensus#Parameters
         // будет отличаться от случая с гомографией
-        const int n_trials = 10000;
+        const int n_trials = 60000;
 
         const double threshold_px = 3;
 
