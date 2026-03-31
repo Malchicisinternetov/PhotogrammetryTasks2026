f9c59d0132c7a6a4e7d1dd97130cf00154580840
Boris <simiyutin.boris@yandex.ru>
2026-03-31 18:11:09 +0300

    Fix CMake build directory usage in install script?

--- .github/scripts/windows/install_ceres_solver.ps1
+++ .github/scripts/windows/install_ceres_solver.ps1
@@ -13,14 +13,12 @@ cd $env:GITHUB_WORKSPACE
 Invoke-WebRequest -Uri "https://github.com/ceres-solver/ceres-solver/archive/2.2.0.zip" -OutFile "2.2.0.zip"
 Expand-Archive -Path "2.2.0.zip" -DestinationPath "."
 cd ceres-solver-2.2.0
-mkdir build -Force | Out-Null
-cd build
 
-cmake .. `
+cmake -S . -B build `
     -DCMAKE_INSTALL_PREFIX="$INSTALL_PREFIX" `
     -DCMAKE_TOOLCHAIN_FILE="C:\vcpkg\scripts\buildsystems\vcpkg.cmake" `
     -DVCPKG_TARGET_TRIPLET="x64-windows" `
     -DUSE_CUDA=OFF
 
-cmake --build . --config Release --parallel
-cmake --install . --config Release
+cmake --build build --config Release --parallel
+cmake --install build --config Release
